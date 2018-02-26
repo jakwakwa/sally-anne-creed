13180be3228ee8d0b587c2b4f8fdefc440bfa0ca
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-02-26 12:50:21 +0200

    removed mailersignupcomponent from page components folder

--- src/app/templates/home/components/mailer-signup/mailer-signup.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-mailer',
-  templateUrl: 'mailer-signup.component.html',
-  styleUrls: ['mailer-signup.component.scss']
-})
-export class MailerSignupComponent {}
