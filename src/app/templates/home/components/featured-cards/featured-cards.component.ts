69d15e601fe26f4484e4182fd25ce2a74d6b7c7f
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-03-06 08:20:30 +0200

    featured cards component cleanup

--- src/app/templates/home/components/featured-cards/featured-cards.component.ts
+++ src/app/templates/home/components/featured-cards/featured-cards.component.ts
@@ -6,20 +6,17 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['featured-cards.component.scss']
 })
 export class FeaturedCardsComponent {
-
-  p: number = 1;
-
   // GET|SET event
   private _content: any;
   get content(): any {
       return this._content;
   }
+  // SET event
   @Input()
   set content(value: any) {
       this._content = value;
   }
-
+  // Type for routing purposes
   @Input()
   type;
-
 }
