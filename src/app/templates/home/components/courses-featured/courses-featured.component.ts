b4f2bdbb044afa2bb906f6014add88b63d22c4ae
Jaco Kotzee <jaco@doodlesdigital.co.za>
2018-03-06 08:17:07 +0200

    courses featured component cleanup

--- src/app/templates/home/components/courses-featured/courses-featured.component.ts
+++ src/app/templates/home/components/courses-featured/courses-featured.component.ts
@@ -1,19 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { CoursesFeatured } from '../../models/courses-featured.interface';
 
-
 @Component({
   selector: 'app-courses-featured',
   templateUrl: 'courses-featured.component.html',
   styleUrls: ['courses-featured.component.scss']
 })
-export class CoursesFeaturedComponent {
 
-  // GET|SET event
+export class CoursesFeaturedComponent {
+  // GET event
   private _courses: Array<CoursesFeatured>;
   get courses(): Array<CoursesFeatured> {
       return this._courses;
   }
+  // SET event
   @Input()
   set courses(value: Array<CoursesFeatured>) {
       this._courses = value;
